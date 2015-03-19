e1d5b02acd6a24d85304d3a5cbfacc0cc970c339
myun2 <myun2@nwhite.info>
2015-03-19 16:53:40 +0900

    header

--- btree.hpp
+++ btree.hpp
@@ -1,6 +1,8 @@
 #ifndef __MYUN2__BTREE__HPP__
 #define __MYUN2__BTREE__HPP__
 
+#include <memory.h>
+
 namespace myun2
 {
 	template <typename Alloc, typename Key, typename Value, unsigned int RecCount=4>
@@ -12,8 +14,22 @@ namespace myun2
 			Key k;
 			Value v;
 		};
+		struct block {
+			struct header {
+				unsigned int used;
+				unsigned int left;
+				unsigned int right;
+				unsigned int _padding;
+			};
+			header head;
+			node nodes[RecCount - 1];
+		};
 		void init() {
-			if ( alc.empty() ) alc.allocate(sizeof(node) * RecCount);
+			if ( alc.empty() ) { allocate(); }
+		}
+		void allocate() {
+			block* p_blk = (block*)alc.allocate(sizeof(node) * RecCount);
+			memset(p_blk, 0x00, sizeof(block));
 		}
 	public:
 		btree(){ init(); }
