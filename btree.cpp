5eaecd17285a2f7012c786da8565bb00b0825b2e
myun2 <myun2@nwhite.info>
2015-03-19 17:23:12 +0900

    file test

--- btree.cpp
+++ btree.cpp
@@ -5,10 +5,20 @@
 
 using namespace myun2;
 
-int main()
+template <typename Bt>
+void btree_test(Bt& bt)
 {
-	btree<btree_::mem_alloc, const char*, const char*> bt;
 	bt.insert("a", "abcdef");
 	bt.insert("d", "dfghij");
+}
+
+int main()
+{
+	btree<btree_::mem_alloc, const char*, const char*> bt_mem;
+	btree<btree_::file_alloc, const char*, const char*> bt_file("btree.db");
+
+	btree_test(bt_mem);
+	btree_test(bt_file);
+
 	return 0;
 }
